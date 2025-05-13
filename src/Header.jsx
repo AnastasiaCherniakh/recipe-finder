158e62b11991e71158486939c8e44a73677b048f
AnastasiaCherniakh <anastayshablake@gmail.com>
2025-05-13 20:50:15 +0200

    Add Header component and styles

---
+++ src/Header.jsx
@@ -0,0 +1,10 @@
+export default function Header() {
+    return (
+        <header>
+            <nav>
+                <img className="img-logo" src="src/assets/logo.png" />
+                <span className="text-logo">Chef Claude</span>
+            </nav>
+     </header>
+    )
+}
