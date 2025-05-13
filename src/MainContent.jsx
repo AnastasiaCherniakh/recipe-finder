14d5b112a0b1cbd9ab8a3dee874ad011bf613ae6
AnastasiaCherniakh <anastayshablake@gmail.com>
2025-05-13 22:10:43 +0200

    Add hover effect on add ingredients button

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -2,7 +2,7 @@ export default function MainContent() {
     return (
         <main>
             <form className="add-ingredient-form">
-                <input type="text" aria-label="Add ingredient" placeholder="e.g. salmon" />
+                <input type="text" aria-label="Add ingredient" placeholder="e.g. tomatoes" />
                 <button>Add Ingredient</button>
             </form>
         </main>
