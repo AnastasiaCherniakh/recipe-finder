908ed9a3435c94e1087c9c9c334f932f821acdca
AnastasiaCherniakh <anastayshablake@gmail.com>
2025-05-15 22:54:59 +0200

    Implement form to add ingredient and display them in a list

--- src/MainContent.jsx
+++ src/MainContent.jsx
@@ -1,10 +1,26 @@
+import { useState } from 'react';
 export default function MainContent() {
+
+    const [ingredients, setIngredients] = useState([]);
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient");
+        setIngredients(prevIngredients => [...prevIngredients, newIngredient]);
+    }
+
+    const ingredientListItem = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ));
+
     return (
         <main>
-            <form className="add-ingredient-form">
-                <input type="text" aria-label="Add ingredient" placeholder="e.g. tomatoes" />
+            <form action={addIngredient} className="add-ingredient-form">
+                <input type="text" name="ingredient" aria-label="Add ingredient" placeholder="e.g. tomatoes" />
                 <button>Add Ingredient</button>
             </form>
+        <ul>
+            {ingredientListItem}
+        </ul>
         </main>
     )
 }
